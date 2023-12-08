0c1194415b3b0cbc963577b7b369459aae8ef77e
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2023-12-08 16:26:02 +0000

    Adding new screen routes

---
+++ weatherly/lib/screens/city_management.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CityManagement extends StatefulWidget {
+  static const String id = 'citymanagement';
+  const CityManagement({super.key});
+
+  @override
+  State<CityManagement> createState() => _CityManagementState();
+}
+
+class _CityManagementState extends State<CityManagement> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
