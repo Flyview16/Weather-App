d8a47b1c7b5b066b28fe6fbd6e31342deae5bb05
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2023-12-12 05:01:28 +0000

    Building homepage UI

--- weatherly/lib/screens/home_screen.dart
+++ weatherly/lib/screens/home_screen.dart
@@ -12,6 +12,213 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("images/background.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Icon(
+                          Icons.grid_view,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.location_on),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Atlanta',
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Icon(
+                          Icons.location_city_outlined,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 3.5,
+                        backgroundColor: Colors.deepOrange,
+                      ),
+                      Text(
+                        ' Updating',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const Image(
+                    width: 240,
+                    height: 240,
+                    image: AssetImage('images/cloud1.png'),
+                  ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '21°',
+                        style: TextStyle(
+                          fontSize: 135,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Thunderstorm',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Monday,17 May',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(
+                              Icons.air,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              '13km/h',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              'Wind',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.water_drop, color: Colors.blue),
+                            Text(
+                              '24%',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              'Humidity',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(
+                              Icons.thunderstorm,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              '87%',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              'Chance of rain',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Today',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              '7 days',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_forward_ios,
+                            size: 18,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
