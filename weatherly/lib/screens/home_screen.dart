f424601604ca1bc406417cf9d8a9dee04baec4a1
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2023-12-14 17:41:36 +0000

    Making screen scrollable and adding listview

--- weatherly/lib/screens/home_screen.dart
+++ weatherly/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,210 +14,281 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/background.jpg"),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SafeArea(
-          child: Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: const Icon(
-                          Icons.grid_view,
-                          color: Colors.black,
-                          size: 30,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          const Icon(Icons.location_on),
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              'Atlanta',
-                              style:
-                                  TextStyle(fontSize: 35, color: Colors.white),
-                            ),
-                          )
-                        ],
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Icon(
-                          Icons.location_city_outlined,
-                          color: Colors.black,
-                          size: 30,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 3.5,
-                        backgroundColor: Colors.deepOrange,
-                      ),
-                      Text(
-                        ' Updating',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                  const Image(
-                    width: 240,
-                    height: 240,
-                    image: AssetImage('images/cloud1.png'),
-                  ),
-                  const Column(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              //blur effect
+              BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 4,
+                  sigmaY: 4,
+                ),
+                child: Container(),
+              ),
+              //child
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.blue.withOpacity(0.5),
+                        Colors.black.withOpacity(0.8),
+                      ]),
+                ),
+                child: Container(
+                  padding:
+                      const EdgeInsets.only(left: 20.0, right: 20.0, top: 38.0),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        '21°',
-                        style: TextStyle(
-                          fontSize: 135,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        'Thunderstorm',
-                        style: TextStyle(
-                          fontSize: 35,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        'Monday,17 May',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.air,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              '13km/h',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                              ),
-                            ),
-                            Text(
-                              'Wind',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Icon(Icons.water_drop, color: Colors.blue),
-                            Text(
-                              '24%',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                              ),
-                            ),
-                            Text(
-                              'Humidity',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Icon(
-                              Icons.thunderstorm,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              '87%',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                              ),
-                            ),
-                            Text(
-                              'Chance of rain',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Today',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
-                      ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           TextButton(
                             onPressed: () {},
-                            child: const Text(
-                              '7 days',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 20,
-                              ),
+                            child: const Icon(
+                              Icons.grid_view,
+                              color: Colors.black,
+                              size: 30,
                             ),
                           ),
-                          const Icon(
-                            Icons.arrow_forward_ios,
-                            size: 18,
-                            color: Colors.grey,
+                          Row(
+                            children: [
+                              const Icon(Icons.location_on),
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Atlanta',
+                                  style: TextStyle(
+                                      fontSize: 35, color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.location_city_outlined,
+                              color: Colors.black,
+                              size: 30,
+                            ),
                           ),
                         ],
                       ),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 3.5,
+                            backgroundColor: Colors.deepOrange,
+                          ),
+                          Text(
+                            ' Updating',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      const Image(
+                        width: 240,
+                        height: 240,
+                        image: AssetImage('images/cloud1.png'),
+                      ),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            '21',
+                            style: TextStyle(
+                              fontSize: 135,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            'Thunderstorm',
+                            style: TextStyle(
+                              fontSize: 35,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'Monday,17 May',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                        width: double.infinity,
+                        child: Divider(
+                          color: Colors.grey,
+                          thickness: 0.5,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                Icon(
+                                  Icons.air,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  '13km/h',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  'Wind',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Icon(Icons.water_drop, color: Colors.blue),
+                                Text(
+                                  '24%',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  'Humidity',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Icon(
+                                  Icons.thunderstorm,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  '87%',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  'Chance of rain',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Today',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  '7 days',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                              const Icon(
+                                Icons.arrow_forward_ios,
+                                size: 18,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 130,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 4,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 100,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.blue),
+                                  borderRadius: BorderRadius.circular(35.0),
+                                ),
+                                child: const Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                      '23°',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.cloud,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      '10:00',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
