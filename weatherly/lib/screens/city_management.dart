726a84bdf8ccd3aefb44d62b33b45fa3d3657eee
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-01-03 14:01:44 +0000

    Adding bottom navigation bar

--- weatherly/lib/screens/city_management.dart
+++ weatherly/lib/screens/city_management.dart
@@ -34,6 +34,32 @@ class _CityManagementState extends State<CityManagement> {
           ),
           centerTitle: true,
         ),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 0,
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
+            selectedFontSize: 18.0,
+            unselectedFontSize: 18.0,
+            selectedItemColor: Colors.white70,
+            unselectedItemColor: Colors.white70,
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Edit',
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.blueGrey,
+                  size: 40.0,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Add',
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.blueGrey,
+                  size: 40.0,
+                ),
+              ),
+            ]),
         body: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
